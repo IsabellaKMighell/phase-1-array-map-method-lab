e9aa77abffa2d52f6c0f8a9eb16c5ae5d7905ccc
Isabella Mighell <ikm4dm@virginia.edu>
2021-08-15 20:39:37 -0400

    Completed Map

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//think of tutorials as two arrays:
+// each line of string is an element 
+// make the line of string into its own array using .split(' ') 
+//then map it with each index[0] as uppercase 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {const words = sentence.split(' ');
+  const captilizedWords = words.map(word => word[0].toUpperCase() + word.slice(1));
+  const arrayOfWordsToString = captilizedWords.join(' ');
+  return arrayOfWordsToString;
+  })
 }
